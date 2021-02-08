7ff22bb2c0a869b6d5eb8793c4b2ba235512264b
Bennett Huffman <bhuffman@andrew.cmu.edu>
2021-02-07 22:21:49 -0500

    passed step 2 tests

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,16 @@
 class Child < ApplicationRecord
+    # relationships
+    has_many :chores
+    has_many :tasks, through: :chores
     
+    validates_presence_of :first_name, :last_name
+
+    # scopes
+    scope :alphabetical, -> { order(:last_name, :first_name) }
+    scope :active, -> { where(active: true) }
+
+    # methods
+    def name
+        return first_name + " " + last_name
+    end
 end
